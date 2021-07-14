a4b978c616c4bac66a5ed4282fb7d74d119dcf8c
Mathius <devachievem@gmail.com>
2021-07-14 14:43:38 -0700

    create index file with subscription in graphql yoga

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// @ts-check
+const { PrismaClient } = require('@prisma/client')
+const { GraphQLServer, PubSub } = require('graphql-yoga');
+
+const { getUserId } = require('./utils');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
+const typeDefs = require('./typeDefs');
+const jwt = require('jsonwebtoken');
+
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link
+}
+
+const pubsub = new PubSub();
+const prisma = new PrismaClient()
+
+// const authenticate = async (resolve, root, args, context, info) => {
+//   console.log('context: ', context.request.headers);
+
+//   let token;
+//   try {
+//       token = jwt.verify(context.request.get("Authorization"), "secret");
+//   } catch (e) {
+//       return new Error("Not authorised");
+//   }
+//   const result = await resolve(root, args, context, info);
+//   return result;
+// };
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: req => ({
+    ...req, 
+    prisma, 
+    pubsub, 
+    // userId: req && req.headers.authorization
+    // ? getUserId(req)
+    // : null
+  }),
+  // middlewares: [authenticate]
+});
+
+const options = {
+  port: 4000
+};
+server.start(options, ({ port }) => {
+  console.log(
+    `Graphql Server started, listening on port ${port} for incoming requests.`,
+  )
+})
